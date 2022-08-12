32a9083f4126c08e3859ad1aa0947a2b00d5cc83
Mamytova Aziza <aa5414774@gmail.com>
2022-08-12 15:51:23 +0600

    DZ3

---
+++ greeting.js
@@ -0,0 +1,60 @@
+// 1.1 : 
+// let checkAge = prompt('сколько вам лет?',18)
+// let welcom = (age > 18) ? () => alert('true') : () => confirm ('родители разрешили?')
+
+// welcom()
+
+
+// 1.2 :  
+// let a = prompt ('введите число');
+// let b = prompt ('введите число');
+// let result = (a + b < 4) ? () => alert('Мало') : () => alert('Много');
+
+//  result()
+
+//1.3
+// let message;
+// if (login == 'Сотрудник') {
+//   message = 'Привет';
+// } else if (login == 'Директор') {
+//   message = 'Здравствуйте';
+// } else if (login == '') {
+//   message = 'Нет логина';
+// } else {
+//   message = '';
+// }
+
+// let message = prompt('Введите логин');
+// let result = (message == 'сотрудник') ? () => alert('Привет') : (message == 'Директор') ? () => alert('Здравствуйте') : (message == '') ? () => alert('Нет логина') : () => alert('');
+
+// result()
+
+//2.1
+// let a = prompt ('введите число')
+// if (a > 0) {
+//     let ggg = function() {
+//         console.log();('1')
+//     }
+// } else {
+//     let ggg = function() {
+//         console.log();('2')
+//     }
+// }
+
+
+//2.2
+// var elements = [
+//     'Hydrogen',
+//     'Helium',
+//     'Lithium',
+//     'Beryllium'
+//   ];
+
+//   let sum = function (elements) {
+//       return (elements.length)
+//   }
+
+//   console.log(sum(elements))
+
+// let sum = (elements) => elements.lenght
+// console.log(elements)
